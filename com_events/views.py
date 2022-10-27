37ad37829bda85508cd21a1dda4ecb1b27e056d3
Felix Tjahyadi <felix.tjahyadi@ui.ac.id>
2022-10-27 22:06:59 +0700

    Community Events TEMP : Calendar and Button

--- com_events/views.py
+++ com_events/views.py
@@ -1,4 +1,19 @@
+import datetime
+from typing import List
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar, month_name
+from datetime import datetime
 
 def show_events(request):
-    return render(request, "com_events.html")
+    year = datetime.now().year
+    month = datetime.now().strftime('%B')
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    context = {
+        "cal" : cal,
+        "month": month,
+        "year" : year,
+    }
+    return render(request, "com_events.html", context)
