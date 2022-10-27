3903253346867e17e38f06dbb6f8e9c26a93aa37
Felix Tjahyadi <felix.tjahyadi@ui.ac.id>
2022-10-27 22:42:20 +0700

    Community Events TEMP : Models V2 and HTML update

--- com_events/models.py
+++ com_events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 100)
+
+class Event(models.Model):
+    name = models.CharField(max_length = 100)
+    date = models.DateTimeField()
+    manager = models.CharField(max_length = 100)
+    description = models.TextField()
+    attendees = models.ManyToManyField(User, blank = True)
--- com_events/views.py
+++ com_events/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar, month_name
 from datetime import datetime
+from com_events.models import *
 
 def show_events(request):
     year = datetime.now().year
@@ -11,9 +12,11 @@ def show_events(request):
     month_number = list(calendar.month_name).index(month)
     month_number = int(month_number)
     cal = HTMLCalendar().formatmonth(year, month_number)
+    event = Event.objects.all()
     context = {
         "cal" : cal,
         "month": month,
         "year" : year,
+        "event" : event,
     }
     return render(request, "com_events.html", context)
