e3c4aca6afb9c0ac4395ac2a92b41e5c39e14f59
vwjaya32 <vinsen.wijaya@ui.ac.id>
2022-10-29 16:38:41 +0700

    Navbar HOMEPAGE

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from homepage.views import index
+from homepage.views import *
 
 app_name = 'homepage'
 
--- homepage/views.py
+++ homepage/views.py
@@ -2,3 +2,38 @@ from django.shortcuts import render
 
 def show_homepage(request):
     return render(request, 'homepage.html')
+
+
+def register_temp(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('com_events:login')
+
+    context = {'form': form}
+    return render(request, 'register_temp.html', context)
+
+
+def login_user_temp(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("com_events:show_events"))
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login_temp.html', context)
+
+
+def logout_user_temp(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('com_events:login'))
+    return response
