2b79174d989a8d8184f75faa537ac541cad39b82
BalqisAsySyifaLumbun <balqislumbun@gmail.com>
2022-10-30 17:32:27 +0700

    Fix JSON

--- mh_tests/views.py
+++ mh_tests/views.py
@@ -11,19 +11,19 @@ from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from django.urls import reverse
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
 
 # Create your views here.
-def username_in(request):
-    username = None
-    if request.user.is_authenticated:
-        username = request.user.username
-        return username
+# def username_in(request):
+#     username = None
+#     if request.user.is_authenticated:
+#         username = request.user.username
+#         return username
 
 @login_required(login_url='/homepage/login/')
 def mhtestpage(request):
     mh_list = MentalTest.objects.filter(user=request.user)
     context = {
-        'user': username_in(request),
         'data': mh_list,
     }
     return render(request, "mhtest.html", context)
@@ -33,19 +33,27 @@ def save_points(request):
     if request.method == 'POST':
         user = request.user
         score = request.POST.get("score")
-        new_result = MentalTest( user=user, score=score, date=datetime.date.today())
+        date = request.POST.get("date")
+        new_result = MentalTest( user=user, score=score, date=date)
         new_result.save()
 
         return JsonResponse(
             {"pk" : new_result.pk, 
             "fields": {
-            "user" : new_result.user,
+            "user" : model_to_dict(new_result.user),
             "score" : new_result.score,
             "date" : new_result.date,
         }})
     return HttpResponseNotFound()
-@login_required(login_url='/homepage/login/')
+
 def json_res(request):
     data = MentalTest.objects.filter(user = request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+@login_required(login_url='/homepage/login/')
+@csrf_exempt
+def delete_res(request, pk):
+    lists=MentalTest.objects.get(id=pk)
+    lists.delete()
+    messages.success(request, "Your result has been deleted.")
+    return redirect ('mh_tests:mhtest')
