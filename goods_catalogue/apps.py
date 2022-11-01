ed4f4f9935b2a8f77f1032a5ad15e054271810f3
thobyasmuda <silalahithobyas@gmail.com>
2022-11-02 01:01:22 +0700

    MH_Goods = goods_catalogue & goods_cart

--- goods_catalogue/apps.py
+++ goods_catalogue/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MhGoodsConfig(AppConfig):
+class GoodsCatalogueConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mh_goods'
+    name = 'goods_catalogue'
