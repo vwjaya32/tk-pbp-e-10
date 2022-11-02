5088ef085ed8d4f8d257a9ca888e3342256354b6
thobyasmuda <silalahithobyas@gmail.com>
2022-11-02 23:55:10 +0700

    Marketplace :(

--- goods_catalogue/admin.py
+++ goods_catalogue/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Catalogue)
+admin.site.register(SoulComforter)
+admin.site.register(AnxietyBoost)
--- goods_catalogue/models.py
+++ goods_catalogue/models.py
@@ -1,32 +1,32 @@
 from django.db import models
 		
 class CatalogueManager(models.Manager):
-	def get_by_natural_key(self, item_name, item_price, imageURL):
-		return self.get(name=item_name, price=item_price, imageURL=imageURL)
+	def get_by_natural_key(self, name, price, imageURL):
+		return self.get(name=name, price=price, imageURL=imageURL)
 
 class Catalogue(models.Model):
-	item_name = models.CharField(max_length=255)
-	item_price = models.FloatField()
-	item_picture = models.ImageField(upload_to='upload/', null=True, blank=True)
+	name = models.CharField(max_length=255)
+	price = models.FloatField()
+	picture = models.ImageField(upload_to='upload/', null=True, blank=True)
 
 	objects = CatalogueManager()
 
 	def __str__(self):
-		return self.item_name
+		return self.name
 
 	@property
 	def imageURL(self):
 		try:
-			url = self.item_picture.url
+			url = self.picture.url
 		except:
 			url = ''
 		return url
 
 	def natural_key(self):
-		return {'name': self.item_name, 
-			'price': self.item_price,
-			'imageURL': self.imageURL,
-			}
+		return {'name': self.name, 
+				'price': self.price,
+				'imageURL': self.imageURL,
+				}
 
 class SoulComforter(Catalogue):
 	max_comfort = models.IntegerField()
@@ -49,8 +49,8 @@ class AnxietyBoost(Catalogue):
 		return super()
 
 	def natural_key(self):
-		return {'name': self.item_name, 
-			'price': self.item_price,
+		return {'name': self.name, 
+			'price': self.price,
 			'imageURL': self.imageURL,
 			}
 
